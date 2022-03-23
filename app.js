416e58692a0e21b5c80c09e5c0650764eaf720a6
Faliell <76968922+Faliell@users.noreply.github.com>
2022-03-23 22:04:04 +0100

    Procfile read

--- app.js
+++ app.js
@@ -15,4 +15,4 @@ else {
 }
 })
 
-app.listen(3000 || process.env.PORT, ()=> console.log("Server working in port 3000"))
+app.listen(process.env.PORT || 3000, ()=> console.log("Server working in port 3000"))
