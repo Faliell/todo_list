c042955aabae370d101593ff4f301c26969e7138
Faliell <76968922+Faliell@users.noreply.github.com>
2022-03-23 21:36:39 +0100

    vs-1

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+
+const app = express()
+
+app.get("/", (req, res)=> {
+var today = new Date()
+var currentDay = today.getDay()
+
+if (currentDay === 6 || currentDay === 0) {
+  res.send("<h1>weekend</h1>")
+}
+else {
+  res.sendFile(__dirname + "/index.html")
+}
+})
+
+app.listen(3000, ()=> console.log("Server working in port 3000"))
