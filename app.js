c3b604f5a9a4a20e85e5d6bbed92bbb7c405c23b
Faliell <76968922+Faliell@users.noreply.github.com>
2022-03-26 18:00:38 +0100

    for loop

--- app.js
+++ app.js
@@ -3,36 +3,33 @@ const bodyParser = require("body-parser")
 
 const app = express()
 
+var items = ["Buy food","Cook food","Eat food"]
+
+app.use(bodyParser.urlencoded({extended:true}))
+
 app.set('view engine', 'ejs');
 
 app.get("/", (req, res)=> {
-var today = new Date()
-var currentDay = today.getDay()
-var day = ""
 
-switch (currentDay) {
-  case 0:
-    day = "Sunday";
-    break;
-  case 1:
-    day = "Monday";
-    break;
-  case 2:
-     day = "Tuesday";
-    break;
-  case 3:
-    day = "Wednesday";
-    break;
-  case 4:
-    day = "Thursday";
-    break;
-  case 5:
-    day = "Friday";
-    break;
-  case 6:
-    day = "Saturday";
-}
-res.render("list",{kindOfDay:day})
+var today = new Date()
+
+var options = { weekday:"long",
+day:"numeric",
+month:"long"}
+
+var day = today.toLocaleDateString("en-US", options)
+
+
+res.render("list",{kindOfDay:day, newListItems:items})
 })
+
+app.post("/", (req,res)=> {
+  var item = req.body.newItem
+  items.push(item)
+
+  res.redirect("/")
+
+})
+
 
 app.listen(process.env.PORT || 3000, ()=> console.log("Server working in port 3000"))
